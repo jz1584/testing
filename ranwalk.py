404c62c5b9db70b3465eaa92607560c3ac00ba3e
jianming Zhou <diymyfuture@gmail.com>
2014-09-27 17:10:36 -0400

    plotting

--- ranwalk.py
+++ ranwalk.py
@@ -1,28 +1,23 @@
 import random
-import matplotlib.pyplot as plt
-position=0
-walk=[position]
-steps=1000
 
-for i in range(steps):
-	a=random.randint(0,1)
-	if a==1:
-		step=1
 
-	elif a==0:
-		step=-1
+#return walk :a list of random cumulative integers 
+def rwalk(steps):
+	position=0
+	walk=[position]
+	for i in range(steps):
+		a=random.randint(0,1)
+		if a==1:
+			step=1
+
+		elif a==0:
+			step=-1
 		
-	else:	
-		print " error"
-	position+=step
-	walk.append(position)
-
-
-
-plt.plot(walk)
-plt.ylabel("walk direction")
-plt.xlabel("walking steps")
-plt.show()
+		else:	
+			print " error"
+		position+=step
+		walk.append(position)
+        return walk
 
 
 
