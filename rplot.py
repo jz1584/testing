5883e30e4f5ab40bccf10c4fadeaa85ea3e5f7f9
jianming Zhou <diymyfuture@gmail.com>
2014-09-27 17:11:29 -0400

    plotting

---
+++ rplot.py
@@ -0,0 +1,14 @@
+import random
+import matplotlib.pyplot as plt
+
+#plotting 2 lines 
+def plot(walk1,walk2):
+	plt.plot(walk1)
+    plt.plot(walk2)
+       
+	plt.ylabel("walk direction")
+	plt.xlabel("walking steps")
+	plt.show()
+
+
+
