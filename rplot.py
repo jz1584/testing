5fe6e84a5e082a93fab1c561d031795653c9a070
jianming Zhou <diymyfuture@gmail.com>
2014-09-27 17:28:16 -0400

    pydev

--- rplot.py
+++ rplot.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 #plotting 2 lines 
 def plot(walk1,walk2):
 	plt.plot(walk1)
-    plt.plot(walk2)
+	plt.plot(walk2)
        
 	plt.ylabel("walk direction")
 	plt.xlabel("walking steps")
